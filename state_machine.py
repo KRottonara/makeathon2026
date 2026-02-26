ac830c1d67946f04688e9b8661ceef82acc6afba
Alexander Sigl <c3465803@uon.edu.au>
2026-02-26 18:42:26 +0100

    Defined events

--- state_machine.py
+++ state_machine.py
@@ -6,3 +6,10 @@ class LitterPuppySM(StateChart):
     close_in = State()
     pick_up = State()
     check_picked_up = State()
+
+    cap_detected = search.to(close_in)
+    cap_lost = close_in.to(search)
+    cap_reached = close_in.to(pick_up)
+    pickup_motion_finished = pick_up.to(check_picked_up)
+    pickup_success = check_picked_up.to(search)
+    pickup_failed = check_picked_up.to(close_in)
