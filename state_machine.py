edf258f5d88a91178dc127bcfc220419b250b0c7
Alexander Sigl <c3465803@uon.edu.au>
2026-02-26 19:05:41 +0100

    Ditched state machine class and just used Enum

--- state_machine.py
+++ state_machine.py
@@ -1,15 +1,55 @@
 # Prototype for robot state machine
-from statemachine import StateChart, State
+from xgolib import XGO
+import time
+import math
+from enum import Enum
 
-class LitterPuppySM(StateChart):
-    search = State(initial = True)
-    close_in = State()
-    pick_up = State()
-    check_picked_up = State()
 
-    cap_detected = search.to(close_in)
-    cap_lost = close_in.to(search)
-    cap_reached = close_in.to(pick_up)
-    pickup_motion_finished = pick_up.to(check_picked_up)
-    pickup_success = check_picked_up.to(search)
-    pickup_failed = check_picked_up.to(close_in)
+class PuppyState(Enum):
+    SEARCH = 1
+    CLOSE_IN = 2
+    PICK_UP = 3
+    CHECK_PICKED_UP = 4
+
+
+def search_callback():
+    return
+
+
+def close_in_callback():
+    return
+
+
+def pick_up_callback():
+    return
+
+
+def check_picked_up_callback():
+    return
+
+
+dog = XGO(port='/dev/ttyAMA0',version="xgomini")
+dog_type='M'
+currentState = PuppyState.SEARCH
+
+while True:
+    # Send frame from video camera
+
+    # Get information from MQTT
+
+    match currentState:
+        case PuppyState.SEARCH:
+            search_callback()
+        case PuppyState.CLOSE_IN:
+            close_in_callback()
+        case PuppyState.PICK_UP:
+            pick_up_callback()
+        case PuppyState.CHECK_PICKED_UP:
+            check_picked_up_callback()
+        case _:
+            print("Something went terribly wrong")
+
+
+
+
+
